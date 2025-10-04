2bb3f5f4b45336411c2a5d64feb15ca5b2326624
Hugo <hugofvaldivia@gmail.com>
2025-10-04 10:52:11 +0200

    Nuevo Script

---
+++ Assets/Curso/11º CV/Example_CV.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Course.CV
+{
+    
+    public class Example_CV : MonoBehaviour
+    {
+        int id;
+        private void Start()
+        {
+            id = 1;
+        }
+    }
+}
